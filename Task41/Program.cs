878a689b6477a1ee8332ddd063e7c004ac6d350d
Zelim <zelya140489@gmail.com>
2023-02-06 00:04:29 +0600

    Добавили и изменили программы

---
+++ Task41/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//
+//  0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество элементов массива: ");
+int elementsCount = Convert.ToInt32(Console.ReadLine());
+
+int NumbersGreaterThanZero(int[] numbersArray)
+{
+    int count = 0;
+    for (int i = 0; i < numbersArray.Length; i++)
+    {
+        if (numbersArray[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+int[] numbersArray = new int[elementsCount];
+int temp;
+for (int i = 0; i < elementsCount; i++)
+{
+    Console.Write($"Введите {i + 1}-е число: ");
+    if (int.TryParse(Console.ReadLine(), out temp))
+        numbersArray[i] = temp;
+    else
+    {
+        Console.WriteLine("Введено некорректное значение");
+        Environment.Exit(0);
+    }
+}
+
+void PrintArray(int[] numbersArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < numbersArray.Length; i++)
+    {
+        if (i < numbersArray.Length - 1)
+            Console.Write($"{numbersArray[i]}, ");
+        else Console.Write($"{numbersArray[i]}");
+    }
+    Console.Write("]");
+}
+
+PrintArray(numbersArray);
+int count = NumbersGreaterThanZero(numbersArray);
+Console.WriteLine($" Количество чисел больше 0 -> {count}");
---
+++ Task43/Program.cs
@@ -0,0 +1,40 @@
+﻿// // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// // значения b1, k1, b2 и k2 задаются пользователем.
+// //
+// // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Write("Введите коэффициент k1: ");
+double k1 = UserInput();
+Console.Write("Введите коэффициент b1: ");
+double b1 = UserInput();
+Console.Write("Введите коэффициент k2: ");
+double k2 = UserInput();
+Console.Write("Введите коэффициент b2: ");
+double b2 = UserInput();
+
+double UserInput()
+{
+    double coefficient;
+    if (double.TryParse(Console.ReadLine(), out coefficient));
+    else
+    {
+        Console.WriteLine("Введено некорректное значение.");
+        Environment.Exit(0);
+    }
+    return coefficient;
+}
+
+    double x = (b1 - b2) / (k1 - k2) * -1;
+    double y = k1 * x + b1;
+ 
+    x = Math.Round(x, 1, MidpointRounding.ToZero);
+    y = Math.Round(y, 1, MidpointRounding.ToZero);
+
+if (k1 != k2)
+{
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+}
+else Console.WriteLine("Коэффициенты координат k1 и k2 совпадают, прямые не пересекаются! ");
+
